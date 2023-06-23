c32af6a7feb07d122f3b8096196e47c1ee6383a6
FeisalKeli <kellyfeisal2000@gmail.com>
2023-06-23 21:13:19 +0300

    lab done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+  validates :name, presence: true 
+  validates :name, uniquness: true
+  vaildates :phone_number, length: {is:10}
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length:{min:250}
+  validates :summary,length:{max:250}
+  validates :category, inclusion:{in:['Fiction','Non-Fiction']}
+  validate :sufficiently_clickbait_title
+  private
+
+  def sufficiently_clickbait_title
+    return if title.blank?
+
+    clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+    unless clickbait_phrases.any? { |phrase| title.include?(phrase) }
+      errors.add(:title, "should be sufficiently clickbait-y")
+    end
+  end
 end
